9353a516b7b03098d00f7de43896d204458a393a
AndyTheDragon <158449635+AndyTheDragon@users.noreply.github.com>
2024-04-03 11:34:26 +0200

    Field klasse og underklasser oprettet og klassediagram opdateret.

---
+++ src/Field.java
@@ -0,0 +1,36 @@
+public class Field {
+    private int ID;
+    private String label;
+    private int cost;
+    private int income;
+    private String option;
+
+    public Field(int id, String label, int cost, int income) {
+        this.ID = id;
+        this.label = label;
+        this.cost = cost;
+        this.income = income;
+    }
+
+    @Override
+    public String toString() {
+        return "felt " + ID + ": " + label;
+    }
+
+    public String onLand(Player p) {
+        String s = p.getName() + " er landet på " + this.toString();
+        return s;
+    }
+     public String processResponse(Player p) {
+        return "";
+     }
+
+     protected String onAccept(Player p) {
+        return "";
+     }
+
+     protected String onReject(Player p) {
+        return "";
+     }
+
+}
---
+++ src/Property.java
@@ -0,0 +1,30 @@
+public class Property extends Field {
+
+    private int serieID;
+
+    public Property(int id, String label, int cost, int income, int serieID){
+        super(id, label, cost, income);
+        this.serieID = serieID;
+
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + serieID;
+    }
+
+    @Override
+    public String onLand(Player p) {
+        return super.onLand(p);
+    }
+
+    @Override
+    protected String onAccept(Player p) {
+        return "";
+    }
+
+    @Override
+    protected String onReject(Player p) {
+        return "";
+    }
+}
