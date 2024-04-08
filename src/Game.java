4166d74e5e41be4617c248bd40c36f2ed3ff1c56
Madihakhan1 <144325902+Madihakhan1@users.noreply.github.com>
2024-04-08 14:38:18 +0200

    Changes player & game

--- src/Game.java
+++ src/Game.java
@@ -9,7 +9,7 @@ public class Game {
     private String playerDataPath = "data/playerData.csv";
     private String fieldDataPath = "data/fieldData.csv";
     private String cardDataPath = "data/cardData.csv";
-   private Dice dice = new Dice();
+    private Dice dice = new Dice();
     private int maxPlayers = 6;
 
 
@@ -35,49 +35,45 @@ public class Game {
     }
 
     private void setup() {
-         String [] fieldData = io.readBoardData(fieldDataPath,40);
-         this.board = new Board(fieldData);
-         System.out.println(board.getField(40));
+        String[] fieldData = io.readBoardData(fieldDataPath, 40);
+        this.board = new Board(fieldData);
+        System.out.println(board.getField(40));
 
-         String [] cardData = io.readBoardData(cardDataPath,46);
-         this.cardDeck = new CardDeck(cardData);
+        String[] cardData = io.readBoardData(cardDataPath, 46);
+        this.cardDeck = new CardDeck(cardData);
     }
-
-
-
-
 
 
     public void createPlayer(String name, int balance) {
-            currentPlayer = new Player(name, balance);
-            this.players.add(currentPlayer);
+        currentPlayer = new Player(name, balance);
+        this.players.add(currentPlayer);
     }
 
-    public void runDialog(){
-        ui.displayMsg("welcome to "+this.name);
+    public void runDialog() {
+        ui.displayMsg("welcome to " + this.name);
 
-        int action=0;
-        while(action != listOfActions.size()){// the quit action is the last action
-         action = ui.promptChoice(listOfActions, "Choose action:");
+        int action = 0;
+        while (action != listOfActions.size()) {// the quit action is the last action
+            action = ui.promptChoice(listOfActions, "Choose action:");
 
-         switch(action){
-             case 1:
-                  //start new game
-                  this.registerPlayers();
-                  this.runGameLoop();
-                  break;
-              case 2:
-                  //Continue (last saved) game
-                  this.loadPlayerData();
-                  this.displayPlayers();
-                  this.runGameLoop();
-                  break;
-             case 3:
-                 //quit game
-                 this.endGame();
-                 break;
+            switch (action) {
+                case 1:
+                    //start new game
+                    this.registerPlayers();
+                    this.runGameLoop();
+                    break;
+                case 2:
+                    //Continue (last saved) game
+                    this.loadPlayerData();
+                    this.displayPlayers();
+                    this.runGameLoop();
+                    break;
+                case 3:
+                    //quit game
+                    this.endGame();
+                    break;
 
-         }
+            }
 
         }
     }
@@ -87,33 +83,35 @@ public class Game {
 
         io.saveData(this.players, playerDataPath);
     }
+
     public void loadPlayerData() {
         ArrayList<String> data = io.readPlayerData(playerDataPath);  //"Tess, 2000"
         // obs: hvis der allerede er startet et nyt spil , og vi så loader flere spillere,
         // vil vi både få spillere fra det nystartede spil og fra det gemte spil i player listen
         players = new ArrayList<>();//clear playerlisten.
-        if(data.size()>0){
-            for (String s:data) {
+        if (data.size() > 0) {
+            for (String s : data) {
                 String[] values = s.split(",");//"Tess, 2000" >> ["Tess", " 2000"]
                 int balance = Integer.parseInt(values[1].trim());
-                 String name = values[0];
-                 createPlayer(name, balance);
+                String name = values[0];
+                createPlayer(name, balance);
             }
 
-        }else{
+        } else {
             registerPlayers();
         }
     }
+
     private void registerPlayers() {
         players = new ArrayList<>();//reset players array, so that a new game may be created mid session
         boolean morePlayers = true;
-        while(morePlayers && players.size() < maxPlayers){
+        while (morePlayers && players.size() < maxPlayers) {
             String name = ui.promptText("Tast spiller navn. 'Q' for at quitte");
-            if(!name.equalsIgnoreCase("Q")){
-                createPlayer(name,0);
+            if (!name.equalsIgnoreCase("Q")) {
+                createPlayer(name, 0);
 
-            }else{
-                if(players.size() > 1) {
+            } else {
+                if (players.size() > 1) {
                     morePlayers = false;
                 }
                 ui.displayMsg("Minimum 2 spillere");
@@ -124,34 +122,35 @@ public class Game {
 
     }
 
-    public void displayPlayers(){
-    for(Player c: players){
-        System.out.println(c);
+    public void displayPlayers() {
+        for (Player c : players) {
+            System.out.println(c);
+        }
     }
-}
- private void runGameLoop(){
+
+    private void runGameLoop() {
         int count = 0;
         String input = "Y";
-        while(input.equalsIgnoreCase("Y")){
+        while (input.equalsIgnoreCase("Y")) {
 
             // todo: var det et dobbelslag?
             //todo: some kind of counter
             currentPlayer = players.get(count);
-            ui.displayMsg("Det er "+currentPlayer.getName()+"'s tur");
+            ui.displayMsg("Det er " + currentPlayer.getName() + "'s tur");
             throwAndMove();
             input = ui.promptText("Fortsæt? Y/N: ");
             count++;
-            if(count == players.size()){
+            if (count == players.size()) {
                 count = 0;
             }
         }
     }
 
-    public void throwAndMove(){
-            int result = dice.rollDiceSum();
+    public void throwAndMove() {
+        int result = dice.rollDiceSum();
 
-            int newPosition = currentPlayer.updatePosition(result);
-            Field f = board.getField(newPosition);
+        int newPosition = currentPlayer.updatePosition(result);
+        Field f = board.getField(newPosition);
 
         //  System.out.println(f);
 
