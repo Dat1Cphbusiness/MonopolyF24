0ae04c48ff0c205a2a8af5289aed813003e4fda4
Madihakhan1 <144325902+Madihakhan1@users.noreply.github.com>
2024-04-09 11:13:32 +0200

    Tax

--- src/Tax.java
+++ src/Tax.java
@@ -5,11 +5,32 @@ public class Tax extends Prison{
 
     @Override
     protected String onAccept(Player p) {
-        return super.onAccept(p);
+
+        p.pay(getCost());
+
+        String s = "Du har betalt";
+
+        return s;
     }
 
     @Override
     protected String onReject(Player p) {
-        return super.onReject(p);
+
+        int saldo = p.getBalance();
+        double toPay = saldo* 0.10;
+        p.pay((int) toPay);
+        String s = " Du har betalt 10%";
+        return s;
+    }
+
+
+    @Override
+    public String onLand(Player p) {
+        String s = super.onLand(p);
+
+        s += "\n Vil du betale fast beløb på " + this.getCost() + "kr? Tast j for Ja \n ( Ellers trækker vi et beløb der svare til 10% af dine aktiver)";
+        return s;
+
+
     }
 }
